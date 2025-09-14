ce7870aaac002a730635431e350e8927834b9960
kumarrishav7 <kumarrishav7@gmail.com>
2025-09-14 10:42:39 +0530

    Code Refactor to resolve connection strings

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using Azure.Storage.Blobs;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
@@ -16,47 +14,13 @@ builder.Services
     .ConfigureFunctionsApplicationInsights();
 
 // --- Resolve Storage connection string ---
-string storageConn;
-var localStorageConn = Environment.GetEnvironmentVariable("BlobStorageConnectionString");
-if (!string.IsNullOrEmpty(localStorageConn))
-{
-    storageConn = localStorageConn;
-}
-else
-{
-    var keyVaultUrl = Environment.GetEnvironmentVariable("KEYVAULT_URL");
-    if (string.IsNullOrEmpty(keyVaultUrl))
-    {
-        throw new InvalidOperationException("KEYVAULT_URL is not configured.");
-    }
-
-    var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-    var secret = client.GetSecret("storageaccountconnstring").Value;
-    storageConn = secret.Value;
-}
+var storageConn = builder.Configuration["AzureWebJobsStorage"];
 
 // Register BlobServiceClient
 builder.Services.AddSingleton(new BlobServiceClient(storageConn));
 
 // --- Resolve Service Bus connection string ---
-string serviceBusConn;
-var localServiceBusConn = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
-if (!string.IsNullOrEmpty(localServiceBusConn))
-{
-    serviceBusConn = localServiceBusConn;
-}
-else
-{
-    var keyVaultUrl = Environment.GetEnvironmentVariable("KEYVAULT_URL");
-    if (string.IsNullOrEmpty(keyVaultUrl))
-    {
-        throw new InvalidOperationException("KEYVAULT_URL is not configured.");
-    }
-
-    var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-    var secret = client.GetSecret("servicebusconnstring").Value;
-    serviceBusConn = secret.Value;
-}
+var serviceBusConn = builder.Configuration["ServiceBusConnectionString"];
 
 // Register ServiceBusClient
 builder.Services.AddSingleton(new ServiceBusClient(serviceBusConn));
